6b034137eb17d99efaf170af974c65a955282674
ViKunn <alisson.espin@epn.edu.ec>
2024-01-12 12:52:14 -0500

    Creando mouse

---
+++ Mouse/src/Mouse.java
@@ -0,0 +1,22 @@
+public class Mouse {
+	private boolean botónDerecho;
+	private boolean botónIzquierdo;
+	private boolean ruedita;
+	private boolean sensorMovimiento;
+
+	public void activarBotónDerecho() {
+		// acción que realiza el botón derecho
+	}
+	public void activarBotónIzquierdo() {
+		// acción que realiza el botón izquierdo
+	}
+	private void rueditaUp(){
+
+	}
+	private void rueditaDown(){
+
+	}
+	public void desplazarRuedita() {
+		// desplazar ruedita dependiendo de ...
+	}
+}
